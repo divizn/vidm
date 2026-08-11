84ff57221936c1aa2fb8e84c66e6c5d5e6ba26e4
phon <hassanjaved186@gmail.com>
2026-08-11 13:14:46 +0100

    feat: add SliderTicks component for labeled slider scales

---
+++ www/src/lib/components/SliderTicks.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let {
+		min,
+		max,
+		ticks
+	}: { min: number; max: number; ticks: { value: number; label: string }[] } = $props();
+
+	function positionPercent(value: number): number {
+		return ((value - min) / (max - min)) * 100;
+	}
+</script>
+
+<div class="relative h-4 w-full text-xs">
+	{#each ticks as tick (tick.value)}
+		<div
+			class="text-muted-foreground absolute top-0 flex -translate-x-1/2 flex-col items-center gap-0.5"
+			style:left={`${positionPercent(tick.value)}%`}
+		>
+			<span class="bg-border h-1.5 w-px"></span>
+			<span class="whitespace-nowrap">{tick.label}</span>
+		</div>
+	{/each}
+</div>
